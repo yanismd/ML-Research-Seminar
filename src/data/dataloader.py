7c13bbf97f3a98e21dfb5ada37abb588b2633bf0
AlexandreChaussard <alexandre.chaussard@telecom-sudparis.eu>
2023-02-13 22:19:41 +0100

    Pre-implementation of GLOW

---
+++ src/data/dataloader.py
@@ -0,0 +1,39 @@
+"""
+Data loader for all datasets
+"""
+
+import numpy as np
+from torchvision import datasets, transforms
+import torch
+
+import scipy.io
+import string
+import pandas as pd
+
+
+def fetch_mnist_loader(
+        n_samples_train=1000,
+        n_samples_test=512,
+        batch_size=256,
+        path_to_data="."
+):
+    mnist_trainset = datasets.MNIST(f'{path_to_data}', train=True, download=True, transform=transforms.ToTensor())
+    mnist_testset = datasets.MNIST(f'{path_to_data}', train=False, download=True, transform=transforms.ToTensor())
+
+    # create data loader with said dataset size
+    mnist_trainset_reduced = torch.utils.data.random_split(
+        mnist_trainset,
+        [n_samples_train, len(mnist_trainset) - n_samples_train]
+    )[0]
+    mnist_train_loader = torch.utils.data.DataLoader(mnist_trainset_reduced, batch_size=batch_size, shuffle=True,
+                                                     drop_last=True)
+
+    # download test dataset
+    mnist_testset_reduced = torch.utils.data.random_split(
+        mnist_testset,
+        [n_samples_test, len(mnist_testset) - n_samples_test]
+    )[0]
+    mnist_test_loader = torch.utils.data.DataLoader(mnist_testset_reduced, batch_size=batch_size, shuffle=True,
+                                                    drop_last=True)
+
+    return mnist_train_loader, mnist_test_loader
